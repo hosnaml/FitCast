14a19534e1ce5a51b9faaf6e1559773dfd28d434
hosnaml <hosna@kth.se>
2024-06-11 21:51:31 +0200

    fit-14 adds weather and exercise components

--- src/Weather-component.jsx
+++ src/Weather-component.jsx
@@ -1,3 +1,4 @@
+import { Container } from '@chakra-ui/react'
 import {
     Accordion,
     AccordionItem,
@@ -7,46 +8,17 @@ import {
     Box
   } from '@chakra-ui/react'
 
-function Weather(props) {
+  import Exercises from './Exercise-components';
 
+  function Weather(props) {
 
   return (
     <div>
-      <Accordion>
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as='span' flex='1' textAlign='left'>
-          Section 1 title
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as='span' flex='1' textAlign='left'>
-          Section 2 title
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
-</Accordion>
+      <Container>
+        time period: 12-16, tempreture: 20, wind speed: 5, Exercises: 
+    <Exercises />
+ 
+    </Container>
     </div>
   );
 }
