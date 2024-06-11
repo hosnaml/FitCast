1a0c6f112d00d3285808e806fc1e55c30a784cf3
hosnaml <hosna@kth.se>
2024-06-11 16:35:08 +0200

    Accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from './Weather-component';
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <p>
-          Edit <code>src/App.js</code> and save to fart.
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+      <Weather />
+      </div>
+    </ChakraProvider>
+    
   );
 }
 
