e971987d20319c25576a308b812f9a143da9df96
hosnaml <hosna@kth.se>
2024-06-13 17:17:57 +0200

    SidebarData and Navbar js

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,7 @@ import {link} from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/fa';
 import { AiOutlineAccountBook, AiOutlineClose } from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
 
 function Navbar(props){
 
@@ -17,9 +18,21 @@ function Navbar(props){
             </div>
             <nav classname={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul classname='nav-menu-items'>
+                    <li classname='navbar-toggle'>
                     <link to="#" classname='menu-bars'>
                         <AiOutlineClose />
                     </link>
+                    </li>
+                    {SidebarData.map((item, index) => {
+                        return(
+                            <li key={index} classname={item.cName}>
+                                <link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         </>
